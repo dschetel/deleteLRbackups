8a816d7d5a3344101f9e0cb0171363d301fd3663
DSchetelig <daniel.schetelig@tuhh.de>
2014-12-06 17:17:54 +0100

    delete -> function

--- deleteLRbackup.py
+++ deleteLRbackup.py
@@ -2,17 +2,17 @@ import os, os.path, time
 import shutil
 
 # specify path
-# path = '/Users/dgs/Desktop/test'
-path = '/Volumes/YosemiteData/Pictures/Lightroom/Backups'
+path = 'path/to/Lightroom/Backups'
 
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
 
-number_of_subfolders = get_immediate_subdirectories(path)
-print number_of_subfolders[0]
+def delete_old_backups(number_of_subfolders, path):
+	if len(number_of_subfolders) > 3:
+		for i in range(0, len(number_of_subfolders)-3):
+			print path+'/'+number_of_subfolders[i]
+			shutil.rmtree(path+'/'+number_of_subfolders[i])
 
-if len(number_of_subfolders) > 3:
-	for i in range(0, len(number_of_subfolders)-3):
-		print path+'/'+number_of_subfolders[i]
-		shutil.rmtree(path+'/'+number_of_subfolders[i])
+number_of_subfolders = get_immediate_subdirectories(path)
+delete_old_backups(number_of_subfolders, path)
